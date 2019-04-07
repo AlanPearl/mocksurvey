e948fdab116fa99e4143225ba550d3a51fb7e516
AlanPearl <alanpearl13@gmail.com>
2019-04-07 15:16:13 -0400

    Dependencies added to setup.py

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # mocksurvey.py
+
+# Author
+___
+- Alan Pearl
+
+# Prerequisites
+___
+- halotools: `pip install halotools`
+- Corrfunc: `pip install Corrfunc` (optional but highly recommended)
+- emcee>=3rc: `pip install emcee==3.0rc2`
+- corner: `pip install corner`
+
+# Installation Instructions
+___
+```
+git clone https://github.com/AlanPearl/mocksurvey
+cd mocksurvey
+python setup.py install
+```
--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ from setuptools import setup
 setup(name="mocksurvey",
       version="0.0.1.dev6",
       description="Some useful tools for conducting realistic mock surveys out of galaxies populated by `halotools` models.",
-      #url="http://github.com/AlanPearl/mocksurvey",
+      url="http://github.com/AlanPearl/mocksurvey",
       author="Alan Pearl",
       author_email="alanpearl@pitt.edu",
-      #license="MIT",
+      license="MIT",
       packages=["mocksurvey"],
+      install_requires = [
+		"halotools",
+		"Corrfunc",
+		"emcee>=3.0rc2",
+		"corner"],
       zip_safe=True,
       )
