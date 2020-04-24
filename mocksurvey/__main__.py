08d96424e96b7bb0532f16d1b8852b734d6de590
AlanPearl <alannpearl@outlook.com>
2020-04-23 23:24:54 -0400

    Bug fix in command line scripts

--- mocksurvey/__main__.py
+++ mocksurvey/__main__.py
@@ -15,22 +15,14 @@ if __name__ == "__main__":
     desc = "You can execute several mocksurvey functions from the command line"
 
     parser = argparse.ArgumentParser(prog="mocksurvey", description=desc)
-    sub = parser.add_subparsers(dest="COMMAND", required=True)
+    sub = parser.add_subparsers(dest="FUNCTION", required=True)
     funcs = {name: script(sub.add_parser(name, help=script.desc))
              for name, script in available_scripts.items()}
 
-    # # Mandatory positional arguments
-    # parser.add_argument("COMMAND", type=str, help="Options: "
-    #                                               f"{set(available_scripts.keys())}")
-    #
-    # # Optional positional arguments
-    # parser.add_argument("ARGS", type=str, default=[], nargs="*",
-    #                     help="Positional arguments for COMMAND")
-
     a = parser.parse_args()
-    if a.COMMAND not in available_scripts:
-        raise IOError(f"Unknown command: \"{a.COMMAND}\"")
+    if a.FUNCTION not in available_scripts:
+        raise IOError(f"Unknown command: \"{a.FUNCTION}\"")
 
     # Run the specified command
-    # argv.pop(0)
-    funcs[a.COMMAND]()
+    argv.pop(0)
+    funcs[a.FUNCTION]()
