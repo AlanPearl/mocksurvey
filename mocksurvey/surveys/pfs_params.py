52a92ed834387b12553653e2ee66d1cfecd43f14
AlanPearl <alannpearl@outlook.com>
2021-02-12 14:49:31 -0500

    Split pfs into pfs_low and pfs_high
    pfs_low has the same y and j cuts as pfs
    previously, but over z = 0.7-1.0. For higher
    redshifts, use pfs_high which has no y cut.

--- mocksurvey/surveys/pfs_params.py
+++ mocksurvey/surveys/pfs_params.py
@@ -1,8 +1,21 @@
 class PFSSurvey:
+    z_samples = {
+        0: {
+            "zrange": [0.7, 1.0],
+            "max_dict": dict(m_y=22.5, m_j=22.8)
+        },
+        1: {
+            "zrange": [1.0, 1.7],
+            "max_dict": dict(m_j=22.8)
+        }
+    }
     sqdeg = 12.0
-    zrange = [0.7, 1.7]
-    max_dict = dict(m_y=22.5, m_j=22.8)
     completeness = 0.7
 
+    def __init__(self, z_sample):
+        self.zrange = self.z_samples[z_sample]["zrange"]
+        self.max_dict = self.z_samples[z_sample]["max_dict"]
 
-pfs = PFSSurvey()
+
+pfs_low = PFSSurvey(0)
+pfs_high = PFSSurvey(1)
